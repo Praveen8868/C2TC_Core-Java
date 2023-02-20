6b7e74a265d9ae74f2d45eb98aa901bffdabbf0c
praveen kumar <praveen kumar@DESKTOP-UQ2CNQ2>
2023-02-20 22:54:58 +0530

    Addition

---
+++ Sample/src/sam/Add.java
@@ -0,0 +1,13 @@
+package sam;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 6;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}
