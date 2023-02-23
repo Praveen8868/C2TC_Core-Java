4920fd35537151989670b73e0a014ccb2799f696
praveen kumar <praveen kumar@DESKTOP-UQ2CNQ2>
2023-02-23 15:05:24 +0530

    Ifdemo

---
+++ Day2/src/sam/Ifdemo.java
@@ -0,0 +1,17 @@
+package sam;
+
+public class Ifdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int age=11;
+       
+       if(age>=18)
+    	   System.out.println("you are eligible for vote");
+       
+	
+	   else
+		   System.out.println("you are not eligible for vote");
+	   }
+		
+}
